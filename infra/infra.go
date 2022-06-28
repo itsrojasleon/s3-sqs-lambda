56bb1e63c1298c2dba54069dee1853659d8eb142
rojasleon <rojasleon.dev@gmail.com>
2022-06-28 11:14:24 -0500

    Finished example

--- infra/infra.go
+++ infra/infra.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	cdk "github.com/aws/aws-cdk-go/awscdk/v2"
+	iam "github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
+	lambda "github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	lambdaEvents "github.com/aws/aws-cdk-go/awscdk/v2/awslambdaeventsources"
-	lambda "github.com/aws/aws-cdk-go/awscdk/v2/awslambdanodejs"
+	lambdaNode "github.com/aws/aws-cdk-go/awscdk/v2/awslambdanodejs"
 	s3 "github.com/aws/aws-cdk-go/awscdk/v2/awss3"
 	s3n "github.com/aws/aws-cdk-go/awscdk/v2/awss3notifications"
 	sqs "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
@@ -39,67 +41,53 @@ func NewInfraStack(scope constructs.Construct, id string, props *InfraStackProps
 
 	bucket.AddEventNotification(s3.EventType_OBJECT_CREATED_PUT, s3n.NewSqsDestination(queue))
 
-	lambdaArn := []string{"arn:aws:lambda:", cdkRegion, ":", cdkAccount, ":", "function:process"}
-
-	// role := iam.NewRole(stack, jsii.String("CoolRoleForLambda"), &iam.RoleProps{
-	// 	AssumedBy: iam.NewCompositePrincipal(
-	// 		iam.NewServicePrincipal(jsii.String("lambda.amazonaws.com"), &iam.ServicePrincipalOpts{}),
-	// 		iam.NewServicePrincipal(jsii.String("sqs.amazonaws.com"), &iam.ServicePrincipalOpts{}),
-	// 	),
-	// 	ManagedPolicies: &[]iam.IManagedPolicy{
-	// 		iam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("service-role/AWSLambdaBasicExecutionRole")),
-	// 		iam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("service-role/AWSLambdaSQSQueueExecutionRole")),
-	// 	},
-	// 	InlinePolicies: &map[string]iam.PolicyDocument{
-	// 		"hahaha": iam.NewPolicyDocument(&iam.PolicyDocumentProps{
-	// 			Statements: &[]iam.PolicyStatement{
-	// 				iam.NewPolicyStatement(&iam.PolicyStatementProps{
-	// 					Effect: iam.Effect_ALLOW,
-	// 					Actions: &[]*string{
-	// 						jsii.String("sqs:ReceiveMessage"),
-	// 					},
-	// 					Resources: &[]*string{
-	// 						jsii.String("*"),
-	// 					},
-	// 				}),
-	// 			},
-	// 		}),
-	// 	},
-	// })
-
-	// role.AddManagedPolicy(
-	// 	iam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("service-role/AWSLambdaBasicExecutionRole")),
-	// )
-	// role.AddManagedPolicy(
-	// 	iam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("service-role/AWSLambdaSQSQueueExecutionRole")),
-	// )
-
-	// fn := lambda.NodejsFunction_FromFunctionAttributes(stack, jsii.String("CoolFunction"), &awslambda.FunctionAttributes{
-	// 	FunctionArn: jsii.String(strings.Join(lambdaArn, "")),
-	// Role:        role,
-	// })
-
-	fn := lambda.NodejsFunction_FromFunctionArn(
-		stack,
-		jsii.String("CoolLambda"),
-		jsii.String(strings.Join(lambdaArn, "")),
+	lambdaArn := fmt.Sprintf(
+		"%s%s%s%s%s%s",
+		"arn:aws:lambda:",
+		cdkRegion,
+		":",
+		cdkAccount,
+		":",
+		"function:process",
 	)
 
-	invokeEventSource := lambdaEvents.NewSqsEventSource(queue, &lambdaEvents.SqsEventSourceProps{})
+	roleArn := fmt.Sprintf(
+		"%s%s%s",
+		"arn:aws:iam::",
+		cdkAccount,
+		":role/s3-sqs-lambda-dev-us-east-1-lambdaRole",
+	)
 
-	fn.AddEventSource(invokeEventSource)
+	role := iam.Role_FromRoleArn(
+		stack,
+		jsii.String("LambdaRole"),
+		jsii.String(roleArn),
+		&iam.FromRoleArnOptions{
+			Mutable: jsii.Bool(false),
+		},
+	)
 
-	cdk.NewCfnOutput(stack, jsii.String("lambda-name"), &cdk.CfnOutputProps{
-		Value: jsii.String(*fn.FunctionName()),
-	})
+	fn := lambdaNode.NodejsFunction_FromFunctionAttributes(
+		stack,
+		jsii.String("CoolLambda"),
+		&lambda.FunctionAttributes{
+			Role:        role,
+			FunctionArn: jsii.String(lambdaArn),
+		},
+	)
+
+	sqsEventSource := lambdaEvents.NewSqsEventSource(queue, &lambdaEvents.SqsEventSourceProps{})
+
+	fn.AddEventSource(sqsEventSource)
 
 	return stack
 }
 
 func main() {
 	app := cdk.NewApp(nil)
+
 	if cdkAccount == "" || cdkRegion == "" {
-		log.Fatal("missing env variables")
+		log.Fatal("Missing environment variables")
 	}
 
 	NewInfraStack(app, "InfraStack", &InfraStackProps{
@@ -111,8 +99,6 @@ func main() {
 	app.Synth(nil)
 }
 
-// env determines the AWS environment (account+region) in which our stack is to
-// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 func env() *cdk.Environment {
 	return &cdk.Environment{
 		Account: jsii.String(cdkAccount),
