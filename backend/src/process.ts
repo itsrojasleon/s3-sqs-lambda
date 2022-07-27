8ff16b4c3ca7ddbd061859d2d07838eaa2a33ace
rojasleon <rojasleon.dev@gmail.com>
2022-07-27 16:48:32 -0500

    Implemented a real world example (almost real)

--- backend/src/process.ts
+++ backend/src/process.ts
@@ -1,19 +1,29 @@
-import { APIGatewayProxyResultV2, SQSEvent } from 'aws-lambda';
+import { SQSBatchResponse, SQSEvent } from 'aws-lambda';
 
-export const handler = async (
-  event: SQSEvent
-): Promise<APIGatewayProxyResultV2> => {
-  try {
-    console.log('processing data...', JSON.stringify(event.Records));
+export const handler = async (event: SQSEvent): Promise<SQSBatchResponse> => {
+  const failedMessageIds: string[] = [];
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({})
-    };
-  } catch (err: any) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ err })
-    };
-  }
+  const promises = event.Records.map(async (record) => {
+    try {
+      const body = JSON.parse(record.body);
+      // do something with config...
+      const config = body.Records[0];
+
+      // Simulate work.
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    } catch (err) {
+      console.log('An error ocurred: ', err);
+      failedMessageIds.push(record.messageId);
+    }
+  });
+
+  await Promise.allSettled(promises).then((values) => {
+    console.log(values);
+  });
+
+  return {
+    batchItemFailures: failedMessageIds.map((id) => ({
+      itemIdentifier: id
+    }))
+  };
 };
