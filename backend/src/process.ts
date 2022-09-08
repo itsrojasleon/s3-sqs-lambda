48f175281d6a490d9d9282c89a2705d31135d525
rojasleon <rojasleon.dev@gmail.com>
2022-09-08 17:39:47 -0500

    Improved solution

--- backend/src/process.ts
+++ backend/src/process.ts
@@ -3,23 +3,19 @@ import { SQSBatchResponse, SQSEvent } from 'aws-lambda';
 export const handler = async (event: SQSEvent): Promise<SQSBatchResponse> => {
   const failedMessageIds: string[] = [];
 
+  console.log('event.Records', event.Records);
+
   const promises = event.Records.map(async (record) => {
     try {
-      const body = JSON.parse(record.body);
-      // do something with config...
-      const config = body.Records[0];
-
       // Simulate work.
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log('Doing some work...');
     } catch (err) {
-      console.log('An error ocurred: ', err);
+      console.error('An error ocurred: ', err);
       failedMessageIds.push(record.messageId);
     }
   });
 
-  await Promise.allSettled(promises).then((values) => {
-    console.log(values);
-  });
+  await Promise.allSettled(promises);
 
   return {
     batchItemFailures: failedMessageIds.map((id) => ({
--- infra/infra.go
+++ infra/infra.go
@@ -1,100 +1,74 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
 
-	cdk "github.com/aws/aws-cdk-go/awscdk/v2"
-	iam "github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
-	lambda "github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
-	lambdaEvents "github.com/aws/aws-cdk-go/awscdk/v2/awslambdaeventsources"
-	lambdaNode "github.com/aws/aws-cdk-go/awscdk/v2/awslambdanodejs"
-	s3 "github.com/aws/aws-cdk-go/awscdk/v2/awss3"
-	s3n "github.com/aws/aws-cdk-go/awscdk/v2/awss3notifications"
-	sqs "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awss3notifications"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
 
 type InfraStackProps struct {
-	cdk.StackProps
+	awscdk.StackProps
 }
 
-var cdkAccount = os.Getenv("CDK_DEFAULT_ACCOUNT")
-var cdkRegion = os.Getenv("CDK_DEFAULT_REGION")
-
-func NewInfraStack(scope constructs.Construct, id string, props *InfraStackProps) cdk.Stack {
-	var sprops cdk.StackProps
+func NewInfraStack(scope constructs.Construct, id string, props *InfraStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
 
 	if props != nil {
 		sprops = props.StackProps
 	}
 
-	stack := cdk.NewStack(scope, &id, &sprops)
+	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	bucket := s3.NewBucket(stack, jsii.String("CoolBucket"), &s3.BucketProps{
+	bucket := awss3.NewBucket(stack, jsii.String("coolBucket"), &awss3.BucketProps{
 		Versioned: jsii.Bool(false),
 	})
 
-	queue := sqs.NewQueue(stack, jsii.String("CoolQueue"), &sqs.QueueProps{})
-
-	bucket.AddEventNotification(s3.EventType_OBJECT_CREATED_PUT, s3n.NewSqsDestination(queue))
-
-	lambdaArn := fmt.Sprintf(
-		"%s%s%s%s%s%s",
-		"arn:aws:lambda:",
-		cdkRegion,
-		":",
-		cdkAccount,
-		":",
-		"function:process",
-	)
-
-	roleArn := fmt.Sprintf(
-		"%s%s%s",
-		"arn:aws:iam::",
-		cdkAccount,
-		":role/s3-sqs-lambda-dev-us-east-1-lambdaRole",
-	)
-
-	role := iam.Role_FromRoleArn(
-		stack,
-		jsii.String("LambdaRole"),
-		jsii.String(roleArn),
-		&iam.FromRoleArnOptions{
-			Mutable: jsii.Bool(false),
-		},
-	)
-
-	fn := lambdaNode.NodejsFunction_FromFunctionAttributes(
-		stack,
-		jsii.String("CoolLambda"),
-		&lambda.FunctionAttributes{
-			Role:        role,
-			FunctionArn: jsii.String(lambdaArn),
-		},
-	)
-
-	sqsEventSource := lambdaEvents.NewSqsEventSource(queue, &lambdaEvents.SqsEventSourceProps{
-		// This will do the magic.
-		MaxBatchingWindow: cdk.Duration_Seconds(jsii.Number(5)),
+	queue := awssqs.NewQueue(stack, jsii.String("coolQueue"), &awssqs.QueueProps{
+		ReceiveMessageWaitTime: awscdk.Duration_Seconds(jsii.Number(5)),
 	})
 
-	fn.AddEventSource(sqsEventSource)
+	role := awsiam.NewRole(stack, jsii.String("lambdaRole"), &awsiam.RoleProps{
+		AssumedBy: awsiam.NewServicePrincipal(
+			jsii.String("lambda.amazonaws.com"),
+			&awsiam.ServicePrincipalOpts{},
+		),
+		ManagedPolicies: &[]awsiam.IManagedPolicy{
+			awsiam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("service-role/AWSLambdaSQSQueueExecutionRole")),
+		},
+	})
+
+	bucket.AddEventNotification(
+		awss3.EventType_OBJECT_CREATED_PUT,
+		awss3notifications.NewSqsDestination(queue),
+	)
+
+	awscdk.NewCfnOutput(stack, jsii.String("lambdaRoleArn"), &awscdk.CfnOutputProps{
+		Value:      role.RoleArn(),
+		ExportName: jsii.String("lambdaRoleArn"),
+	})
+
+	awscdk.NewCfnOutput(
+		stack,
+		jsii.String("coolQueueArn"),
+		&awscdk.CfnOutputProps{
+			Value:      queue.QueueArn(),
+			ExportName: jsii.String("coolQueueArn"),
+		})
 
 	return stack
 }
 
 func main() {
-	app := cdk.NewApp(nil)
-
-	if cdkAccount == "" || cdkRegion == "" {
-		log.Fatal("Missing environment variables")
-	}
+	app := awscdk.NewApp(nil)
 
 	NewInfraStack(app, "InfraStack", &InfraStackProps{
-		cdk.StackProps{
+		awscdk.StackProps{
 			Env: env(),
 		},
 	})
@@ -102,9 +76,9 @@ func main() {
 	app.Synth(nil)
 }
 
-func env() *cdk.Environment {
-	return &cdk.Environment{
-		Account: jsii.String(cdkAccount),
-		Region:  jsii.String(cdkRegion),
+func env() *awscdk.Environment {
+	return &awscdk.Environment{
+		Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+		Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
 	}
 }
